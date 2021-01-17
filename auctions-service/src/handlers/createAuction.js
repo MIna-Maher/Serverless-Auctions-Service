7bf218f07addd0e53d147dad058ebcb905031055
Mina <mina.rofaael@vodafone.com>
2021-01-17 10:48:20 +0200

    Adding New files

--- auctions-service/src/handlers/createAuction.js
+++ auctions-service/src/handlers/createAuction.js
@@ -1,12 +1,20 @@
+import { v4 as auctionId  } from 'uuid';  //importing V4 from UUID and assign it to auctionID
+import AWS from 'aws-sdk';
+const DocumentClient = new AWS.DynamoDB.DocumentClient(); //DocumentClient fot Connecting to DynamoDB and insert tables
 async function createAuction(event, context) { // those arguments will be provided when lambda executed.
   const { title } = JSON.parse(event.body);
   const now = new Date();
   //converts the response from Json string into Java array of objects
   const auction = {  //define new object for the auction
     title,
+    id: auctionId(),
     status: 'OPEN',
     createdAt: now.toISOString(),
   };
+  await DocumentClient.put( {
+    TableName : 'AuctionsTable',
+    Item : auction,
+  }).promise();
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
